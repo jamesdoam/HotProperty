afe214824b37ccde7ecb087d4430c9f857c2af2e
thanhatmx3 <thanh@mx3diagnostics.com>
2022-11-15 15:51:14 +1100

    Add response types with status codes

--- HotProperty_PropertyAPI/Controllers/PropertyAPIController.cs
+++ HotProperty_PropertyAPI/Controllers/PropertyAPIController.cs
@@ -11,15 +11,30 @@ namespace HotProperty_PropertyAPI.Controllers
     public class PropertyAPIController : ControllerBase
     {
         [HttpGet]
-        public IEnumerable<PropertyDTO> GetProperties()
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        public ActionResult<IEnumerable<PropertyDTO>> GetProperties()
         {
-            return PropertyStore.propertyList;
+            return Ok(PropertyStore.propertyList);
 
         }
         [HttpGet("{id:int}")]
-        public PropertyDTO GetProperty(int id)
+        [ProducesResponseType(StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public ActionResult<PropertyDTO> GetProperty(int id)
         {
-            return PropertyStore.propertyList.FirstOrDefault(u => u.Id == id);
+            if (id == 0)
+            {
+                return BadRequest();
+            }
+
+            var property = PropertyStore.propertyList.FirstOrDefault(u => u.Id == id); 
+            if (property==null)
+            {
+                return NotFound();
+            }
+
+            return Ok(property);
         }
     }
 }
