64aaeb88c014896efb3b79826d939aab1c397830
thanhatmx3 <thanh@mx3diagnostics.com>
2022-11-15 13:56:53 +1100

    Initial Repo with Basic controller

---
+++ HotProperty_PropertyAPI/Controllers/PropertyAPIController.cs
@@ -0,0 +1,21 @@
+﻿using HotProperty_PropertyAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HotProperty_PropertyAPI.Controllers
+{
+    [ApiController]
+    [Route("api/PropertyAPI")]
+    public class PropertyAPIController : ControllerBase
+    {
+        [HttpGet]
+        public IEnumerable<Property> GetProperties()
+        {
+            return new List<Property>()
+            {
+                new Property{Id=1, Name = "34 Arnold St"},
+                new Property{Id=2, Name = "14 Davey St"}
+            };
+
+        }
+    }
+}
---
+++ HotProperty_PropertyAPI/Models/Property.cs
@@ -0,0 +1,8 @@
+﻿namespace HotProperty_PropertyAPI.Models
+{
+    public class Property
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+    }
+}
