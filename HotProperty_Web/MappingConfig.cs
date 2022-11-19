b7babcd33c634cc8333c828c72ddc8d39980fc42
jamesdoam <jamesdoam@gmail.com>
2022-11-20 08:35:54 +1100

    Add AutoMapper, get ready for creating service

---
+++ HotProperty_Web/MappingConfig.cs
@@ -0,0 +1,17 @@
+﻿using AutoMapper;
+using HotProperty_Web.Models.Dto;
+
+namespace HotProperty_Web
+{
+    public class MappingConfig : Profile
+    {
+        public MappingConfig()
+        {
+            CreateMap<PropertyDTO, PropertyCreateDTO>().ReverseMap();
+            CreateMap<PropertyDTO, PropertyUpdateDTO>().ReverseMap();
+            
+            CreateMap<PropertyNumberDTO, PropertyNumberCreateDTO>().ReverseMap();
+            CreateMap<PropertyNumberDTO, PropertyNumberUpdateDTO>().ReverseMap();
+        }
+    }
+}
